a4fd054328b94d9b75cbfb4babab2ab1ff13da4d
Akshat saxena <akshatsaxena2504@gmail.com>
2025-07-17 07:41:13 +0530

    Initial commit

---
+++ Assignment01 - Tutedude/Math-ops.py
@@ -0,0 +1,7 @@
+A = int(input("Enter the first number: "))
+B = int(input("Enter the second number: "))
+
+print("Addition :", A + B)
+print("Subtraction :", A - B)
+print("Multiplication :", A * B)
+print("Division :", A / B)
---
+++ Assignment01 - Tutedude/Prsnl-greet.py
@@ -0,0 +1,4 @@
+a = input("Enter a first name:")
+b = input("Enter a last name:")
+
+print("Hello",a,b,'!',"Welcome to the Python program.")
